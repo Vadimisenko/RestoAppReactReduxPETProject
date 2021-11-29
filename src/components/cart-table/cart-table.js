655338f9da03021de96bf1fd2976229f4c560b46
Vadym Isenko <vigmail@ukr.net>
2021-11-29 11:51:11 +0200

    Initial commit

---
+++ src/components/cart-table/cart-table.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteFromCard } from '../../actions';
+import './cart-table.scss';
+
+const CartTable = ({items, deleteFromCard}) => {
+    return (
+        <>
+            <div className="cart__title">Ваш заказ:</div>
+            <div className="cart__list">
+                {
+                    items.map(item => {
+                        const {title, price, url, id} = item;
+                        return (
+                            <div key={id} className="cart__item">
+                             <img src={url} className="cart__item-img" alt={title}></img>
+                             <div className="cart__item-title">{title}</div>
+                             <div className="cart__item-price">{price}$</div>
+                             <div onClick={() => deleteFromCard(id)} className="cart__close">&times;</div>
+                        </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+    );
+};
+
+const mapStateToProps = ({items}) => {
+    return {
+        items
+    }
+}
+
+const mapDispatchToProps = {
+    deleteFromCard
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartTable);
